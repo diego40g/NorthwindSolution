29d62fee68ecfedc035533cf53328ec5b1ba3969
diego40g <diegopaznaula@hotmail.com>
2023-07-28 03:26:41 -0500

    deleting a extra librarys and fucntions of doesn't use

--- Northwind.WebApi/Program.cs
+++ Northwind.WebApi/Program.cs
@@ -1,17 +1,10 @@
-using Microsoft.AspNetCore.Routing.Constraints;
 using Northwind.DataAccess;
 using Northwind.UnitOfWork;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-/*if (!builder.Services.Any(x => x.ServiceType == typeof(IntRouteConstraint)))
-{
-    builder.Services.Configure<RouteOptions>(options =>
-    {
-        options.ConstraintMap.Add("int", typeof(IntRouteConstraint));
-    });
-}*/
+
 builder.Services.AddControllers();
 
 builder.Services.AddSingleton<IUnitOfWork>(option => new NorthwindUnitOfWork(
@@ -32,15 +25,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseRouting(); // This should come first
-
-app.UseEndpoints(endpoints =>
-{
-    // Map your endpoints here
-    endpoints.MapControllerRoute(
-        name: "default",
-        pattern: "{controller}/{action}/{id?}",
-        defaults: new { controller = "api", action = "Customer" });
-});
 
 app.UseHttpsRedirection();
 
