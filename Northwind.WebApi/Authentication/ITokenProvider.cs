ed00681472c7826846411e0d38f5389593253189
diego40g <diegopaznaula@hotmail.com>
2023-08-01 23:21:38 -0500

    adding a interfaces of create and valdation the parameter of tokens

---
+++ Northwind.WebApi/Authentication/ITokenProvider.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Northwind.Models;
+
+namespace Northwind.WebApi.Authentication
+{
+    public interface ITokenProvider
+    {
+        string CreateToken(User user, DateTime expiry);
+        TokenValidationParameters GetValidationParameters();
+    }
+}
