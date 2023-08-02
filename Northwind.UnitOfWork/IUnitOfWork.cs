2cccc4621274067ce52a5e31b98d16bcd2e13b11
diego40g <diegopaznaula@hotmail.com>
2023-08-01 23:37:48 -0500

    Adding Interface of user for call into interface of unit works

--- Northwind.UnitOfWork/IUnitOfWork.cs
+++ Northwind.UnitOfWork/IUnitOfWork.cs
@@ -1,14 +1,10 @@
 ﻿using Northwind.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Northwind.UnitOfWork
 {
     public interface IUnitOfWork
     {
         ICustomerRepository Customer { get; }
+        IUserRepository User { get; }
     }
 }
