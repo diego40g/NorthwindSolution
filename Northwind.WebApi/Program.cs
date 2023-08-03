5ff761bf7bf8dea8987a0a246c15578941be5b1b
diego40g <diegopaznaula@hotmail.com>
2023-08-02 23:52:52 -0500

    adding a app service and building to manager the jwt

--- Northwind.WebApi/Program.cs
+++ Northwind.WebApi/Program.cs
@@ -1,5 +1,7 @@
 using Northwind.DataAccess;
 using Northwind.UnitOfWork;
+using Northwind.WebApi.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +12,22 @@ builder.Services.AddControllers();
 builder.Services.AddSingleton<IUnitOfWork>(option => new NorthwindUnitOfWork(
     option.GetRequiredService<IConfiguration>().GetConnectionString("Northwind")
 ));
+
+var tokenProvider = new JwtProvider("issuer", "audience", "northwind_2023");
+builder.Services.AddSingleton<ITokenProvider>(tokenProvider);
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters = tokenProvider.GetValidationParameters();
+    });
+builder.Services.AddAuthorization(auth =>
+{
+    auth.DefaultPolicy = new Microsoft.AspNetCore.Authorization.AuthorizationPolicyBuilder()
+        .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+        .RequireAuthenticatedUser()
+        .Build();
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -23,6 +41,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseAuthentication();
 
 app.UseRouting(); // This should come first
 
