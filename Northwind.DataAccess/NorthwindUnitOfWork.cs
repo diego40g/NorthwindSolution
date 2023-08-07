e1fc8aecff10908cf8f7d08b5538591b87e84b99
diego40g <diegopaznaula@hotmail.com>
2023-08-07 00:12:49 -0500

    updating the iunit with the supplier repository

--- Northwind.DataAccess/NorthwindUnitOfWork.cs
+++ Northwind.DataAccess/NorthwindUnitOfWork.cs
@@ -5,13 +5,16 @@ namespace Northwind.DataAccess
 {
     public class NorthwindUnitOfWork : IUnitOfWork
     {
-        public NorthwindUnitOfWork(string connectionString) 
+        public NorthwindUnitOfWork(string connectionString)
         {
             Customer = new CustomerRepository(connectionString);
-            User = new  UserRepository(connectionString);
+            User = new UserRepository(connectionString);
+            Supplier = new SupplierRepository(connectionString);
         }
         public ICustomerRepository Customer { get; private set; }
 
         public IUserRepository User { get; private set; }
+
+        public ISupplierRepository Supplier { get; private set; }
     }
 }
