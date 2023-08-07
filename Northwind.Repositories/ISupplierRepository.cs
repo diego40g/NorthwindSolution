f2acfd5a90acf35cb98c525de9b80c9915038097
diego40g <diegopaznaula@hotmail.com>
2023-08-06 23:43:07 -0500

    adding a new interfaces to manage supplier

---
+++ Northwind.Repositories/ISupplierRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Northwind.Models;
+
+namespace Northwind.Repositories
+{
+    public interface ISupplierRepository: IRepository<Supplier>
+    {
+        IEnumerable<Supplier> SupplierPagedList(int page, int rows);
+    }
+}
