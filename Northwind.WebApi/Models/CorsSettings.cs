9cd17999ca6fe7abce2a300ddc7c03dfa58105bf
diego40g <diegopaznaula@hotmail.com>
2023-08-20 00:05:32 -0500

    adding class to manage cors

---
+++ Northwind.WebApi/Models/CorsSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Northwind.WebApi.Models
+{
+    public class CorsSettings
+    {
+        public string[] AllowedOrigins { get; set; }
+        public string[] AllowedHeaders { get; set; }
+        public string[] AllowedMethods { get; set; }
+    }
+}
