413d22f9c4f3fe7fb602f99d9d7ec8a5ac481b25
diego40g <diegopaznaula@hotmail.com>
2023-09-01 00:25:07 -0500

    adding an interfaces for pagination method

---
+++ Northwind.Repositories/IOrderRepository.cs
@@ -0,0 +1,10 @@
+﻿using Northwind.Models;
+using System.Collections.Generic;
+
+namespace Northwind.Repositories
+{
+    public interface IOrderRepository: IRepository<Order>
+    {
+        IEnumerable<OrderList> getPaginatedOrder(int page, int rows);
+    }
+}
