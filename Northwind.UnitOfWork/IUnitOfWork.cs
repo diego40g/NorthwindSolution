2eca2a30a511ceb5cf5f755e3b790f9bbab669de
diego40g <diegopaznaula@hotmail.com>
2023-09-01 00:25:49 -0500

    adding a requerst using the unit work

--- Northwind.UnitOfWork/IUnitOfWork.cs
+++ Northwind.UnitOfWork/IUnitOfWork.cs
@@ -7,5 +7,6 @@ namespace Northwind.UnitOfWork
         ICustomerRepository Customer { get; }
         IUserRepository User { get; }
         ISupplierRepository Supplier { get; }
+        IOrderRepository Order { get; }
     }
 }
---
+++ Northwind.WebApi/Controllers/OrderController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Northwind.UnitOfWork;
+
+namespace Northwind.WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Order")]
+    [Authorize]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        [Route("GetPaginatedOrder/{page:int}/{rows:int}")]
+        public IActionResult GetPaginatedOrders(int page, int rows)
+        {
+            return Ok(_unitOfWork.Order.getPaginatedOrder(page, rows));
+        }
+    }
+}
